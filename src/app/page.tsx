87be1fafec1fbe6b792065af9563e90860174922
matheus jesse <matheus_jesse@hotmail.com>
2024-01-06 19:31:21 -0300

    feat: dando enfase na palavra do texto da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <ContainerPage>
       <Header />
       <h1>Tutoriais de Programação</h1>
-      <p>Aqui você encontrará assuntos relacionados a <span className='highlight'>programação</span> de uma maneira fácil de entender. Temos dicas, truques, exemplos e tutoriais em profundidade <span className='highlight'>para ajudar você</span> a expandir seu conhecimento.</p>
+      <p>Aqui você encontrará assuntos relacionados a <span className='highlight'>programação</span> de uma maneira <strong>fácil de entender</strong>. Temos dicas, truques, exemplos e tutoriais em profundidade <span className='highlight'>para ajudar você</span> a expandir seu conhecimento.</p>
       <hr/>
       <p>Então, dê uma olhada e aproveite para aprender. E fique ligado, porque sempre há novidades por aqui!</p>
     </ContainerPage>
