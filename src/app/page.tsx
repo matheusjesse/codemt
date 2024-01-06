703ba7cd4a29b0f9d347a0926d901eacd7c711a8
matheus jesse <matheus_jesse@hotmail.com>
2024-01-06 19:55:53 -0300

    feat: chamando o componente card na home do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Card from '@/components/Card/indext';
 import Header from '@/components/Header';
 import { ContainerPage } from '@/styles/page';
 
@@ -10,6 +11,10 @@ export default function Home() {
         <p>Aqui você encontrará assuntos relacionados a <span className='highlight'>programação</span> de uma maneira <strong>fácil de entender</strong>. Temos dicas, truques, exemplos e tutoriais em profundidade <span className='highlight'>para ajudar você</span> a expandir seu conhecimento.</p>
         <hr/>
         <p>Então, dê uma olhada e aproveite para aprender. E fique ligado, porque sempre há novidades por aqui!</p>
+      </section>
+      <hr/>
+      <section>
+        <Card />
       </section>
     </ContainerPage>
   )
