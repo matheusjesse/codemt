44a778708e6f098c1666c5fedb7160e3b2a6782d
matheus jesse <matheus_jesse@hotmail.com>
2024-01-07 18:21:34 -0300

    chore: alterando o tamanho da home do projeto

--- src/styles/page.ts
+++ src/styles/page.ts
@@ -3,7 +3,7 @@
 import styled from 'styled-components';
 
 export const ContainerPage = styled.main`
-    height: 100%;
+    height: 100vh;
     display: flex;
     justify-content: center;
     flex-wrap: wrap;
