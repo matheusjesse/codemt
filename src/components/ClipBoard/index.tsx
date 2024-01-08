c8f96dfb188a540150107fa659e1f27b77de57cb
matheus jesse <matheus_jesse@hotmail.com>
2024-01-07 21:09:57 -0300

    feat: criando o botão clip board para copiar os comandos dos tutoriais

---
+++ src/components/ClipBoard/index.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { ClipBoardContainer } from './style';
+import { useState } from 'react';
+import CopyImage from '../../../public/copy.png';
+import CheckCircle from '../../../public/check-circle-2.png';
+import Image from 'next/image';
+
+interface propCode {
+    codeText: string
+}
+
+function defaultImage () {
+    return(
+        <Image className="default-image" src={CopyImage} alt="imagem de 2 quadrados sobreprondo o outro representando um clipboard"/>
+    )
+}
+
+function confirmImage () {
+    return(
+        <Image className="confirm-image" src={CheckCircle} alt="Imagem de um circulo com uma sinbulo de V dentro representando que a ação foi concluida"/>
+    )
+}
+
+export default function ClipBoard ({codeText}: propCode) {
+    const [copiado, setCopiado] = useState(false);
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(codeText)
+        .then(() => {
+            setCopiado(true);
+            setTimeout(() => {
+                setCopiado(false);
+            }, 2000);
+        })
+        .catch((err) => {
+            console.error('Erro ao copiar para a área de transferência', err);
+        });
+      };
+
+    return(
+        <ClipBoardContainer>
+            <pre>
+                <code>
+                    {codeText}
+                </code>
+                <button className='clip-board-button' onClick={copyToClipboard}>
+                    {copiado && (<span className='tooltip'>Copiado!</span>) }
+                    {!copiado ? defaultImage() : confirmImage()}
+                </button>
+            </pre>
+        </ClipBoardContainer>
+    )
+}
---
+++ src/components/ClipBoard/style.tsx
@@ -0,0 +1,47 @@
+'use client'
+import styled from 'styled-components'
+
+export const ClipBoardContainer = styled.div`
+
+
+    .clip-board-button {
+        background: none;
+        border: none;
+        display: flex;
+        align-items: center;
+
+    }
+
+    .default-image { 
+        width: 30px;
+        height: 30px;
+        border-radius: 4px;
+        padding: 4px;
+    }
+
+    .default-image:active {
+        padding: 4px;
+        border-radius: 8px;
+    }
+
+    .confirm-image {
+        width: 30px;
+        height: 30px;
+        border-radius: 4px;
+        padding: 4px;
+    }
+
+    .tooltip {
+        background-color: #584d4d;
+        width: 56px;
+        height: 20px;
+        border-radius: 4px;
+        font-size: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #e7e4e4;
+        box-shadow: 1px 1px 0px 0px rgb(4, 1, 4, 0.2);
+        font-family: 'Lexend', sans-serif;
+    }
+`
